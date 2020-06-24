4dafad7680d4d9c043693944ff64c86dba60fbf7
nicoacos7a <nicoacos7a@gmail.com>
2020-06-24 20:39:27 -0300

    1 file modified

--- src/app/components/equipos/equipos.component.ts
+++ src/app/components/equipos/equipos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Equipo } from "../../models/equipo";
 import { EquiposService } from "../../services/equipos.service";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ModalDialogService } from "../../services/modal-dialog.service";
 
 @Component({
   selector: 'app-equipos',
@@ -10,7 +11,31 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class EquiposComponent implements OnInit {
 
-  constructor() { }
+  Titulo = "Equipos";
+  TituloAccionABMC = {
+    A: "(Agregar)",
+    B: "(Eliminar)",
+    M: "(Modificar)",
+    C: "(Consultar)",
+    L: "(Listado)"
+  };
+  AccionABMC = "L"; // inicialmente inicia en el listado de articulos (buscar con parametros)
+
+  Mensajes = {
+    SD: " No se encontraron registros...",
+    RD: " Revisar los datos ingresados..."
+  };
+
+  ListaEquipos: Equipo[] = [];
+  SinBusquedasRealizadas = true;
+  FormReg: FormGroup;
+  submitted = false;
+
+  constructor(
+    private equiposService: EquiposService,
+    public formBuilder: FormBuilder,
+    private modalDialogService: ModalDialogService
+  ) { }
 
   ngOnInit() {
   }
